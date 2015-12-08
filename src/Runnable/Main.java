8194e828bf38b480c30b61748b9adc50b4a8cb9e
Dildo__Queen <Dildo__Queen@DildoQueen>
2015-12-09 00:50:40 +0800

    Julio Cesar Marin
    Facturador simple
    Cuador de dialogo: Archivo corrupto

--- src/Runnable/Main.java
+++ src/Runnable/Main.java
@@ -6,12 +6,15 @@ import Gui.CrearInstancia;
 import Gui.Login;
 import Obj.Kardex;
 import java.io.BufferedWriter;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class Main {
 
@@ -34,6 +37,7 @@ public class Main {
                     System.out.println("Archivo existe");
                     ois = new ObjectInputStream(new FileInputStream(us + "/Documentos/Kardez.dat"));
                     krdx = (Kardex) ois.readObject();
+                    JOptionPane.showMessageDialog(null, "Archivos binarios curruptos, por favor eliminar el archivo C:\\Kardex.dat", "Crear Instancia", JOptionPane.ERROR_MESSAGE);
                 } else {
                     bf = new BufferedWriter(new FileWriter(fl));
                     new CrearInstancia().setVisible(true);
@@ -53,6 +57,7 @@ public class Main {
             }
         } catch (Exception e) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+            JOptionPane.showMessageDialog(null, "Datos binarios curruptos, por favor eliminar el archivo C:\\Kardez.dat\n" + e.getLocalizedMessage() + "\n" + e.getMessage(), "Crear instancia", JOptionPane.ERROR_MESSAGE);
         }
         if (krdx != null) {
             new Login(krdx).setVisible(true);
